4b738d2d317b8c8aedbd8938df8896d4e802d2f8
Apprentice <apprentice@dbc15.local>
2013-11-20 15:29:31 -0800

    initial commit w rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoOrDontButDo::Application.config.secret_token = '6aa476315bcb157c313f0ec244b47886f6d8e8dd99a4c7e9f7baf78492d790221a6cc88a2ca24f9b59134ee6919af990d648da80d00e6762d38c7ccc9b78df94'
