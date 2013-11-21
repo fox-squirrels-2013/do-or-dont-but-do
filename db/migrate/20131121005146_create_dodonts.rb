b4beec6585f86f5507fd7ae832784363f7e6e75f
= <=>
2013-11-20 16:53:54 -0800

    Renamed migration file, updated schema accordingly

--- db/migrate/20131121005146_create_dodonts.rb
+++ db/migrate/20131121005146_create_dodonts.rb
@@ -1,6 +1,6 @@
-class CreateDoDonts < ActiveRecord::Migration
+class CreateDodonts < ActiveRecord::Migration
   def change
-    create_table :do_donts do |t|
+    create_table :dodonts do |t|
       t.string :content
       t.integer :dos, default: 0
       t.integer :donts, default: 0
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131120235446) do
+ActiveRecord::Schema.define(:version => 20131121005146) do
 
-  create_table "do_donts", :force => true do |t|
+  create_table "dodonts", :force => true do |t|
     t.string  "content"
     t.integer "dos",     :default => 0
     t.integer "donts",   :default => 0
