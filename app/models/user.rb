97f79a688949afe03a7463efa159efc2d96123bb
= <=>
2013-11-21 14:35:14 -0800

    add more tests for model - changing context for tests - adding validations - adding CRUD operations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   validates :name, :password, :email, presence: true
+  validates :email, uniqueness: true
+  validate :email_is_of_valid_format
+
+  def email_is_of_valid_format
+    unless self.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      self.errors[:email] << "is not a valid email"
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,82 @@
 require 'spec_helper'
 
 describe User do
-  it "can be created in the database" do
+  before :each do
     @user = User.new
-    expect(@user).to be_an_instance_of User
+    @user.name = 'Beni'
+    @user.password = '123'
+    @user.email = 'b@g.com'
   end
 
-  it "validate presence of name, email and password" do
-    @user = User.new
-    expect{
-      @user.name = ''
-      @user.save!
-    }.to raise_error ActiveRecord::RecordInvalid
+  context "model" do
+    it "should be an instance of the User class" do
+      expect(@user).to be_an_instance_of User
+    end
+  end
 
-    @user = User.new
-    expect{
-      @user.password = ''
-      @user.save!
-    }.to raise_error ActiveRecord::RecordInvalid
+  context "CRUD operations" do
+    before :each do
+      @user.save
+    end
 
-    @user = User.new
-    expect{
-      @user.email = ''
-      @user.save!
-    }.to raise_error ActiveRecord::RecordInvalid
+    it "should delete a record from the database" do
+      expect{@user.delete}.to change{User.count}.by(-1)
+    end
+
+    it "should update a record in the database" do
+      expect{
+        @user.name = 'Dan'
+        @user.save
+        }.to change{User.last.name}.from('Beni').to('Dan')
+    end
+
+    it "should find a record in the database" do
+      expect(User.find(@user.id)).to be_an_instance_of User
+    end
 
   end
+
+  context "validations" do
+    it "should validate presence of name" do
+      expect{
+        @user.name = ''
+        @user.save!
+        }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it "should validate presence of password" do
+      expect{
+        @user.email = ''
+        @user.save!
+        }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    context "for email" do
+      it "should validate presence" do
+        expect{
+          @user.password = ''
+          @user.save!
+          }.to raise_error ActiveRecord::RecordInvalid
+      end
+
+      it "should validate uniqueness" do
+        expect{
+          @user.save
+          @user2 = User.new
+          @user2.name = 'Dan'
+          @user2.password = '123'
+          @user2.email = @user.email
+          @user2.save!
+          }.to raise_error ActiveRecord::RecordInvalid
+      end
+      it "should validate proper format" do
+        expect{
+          @user.email = 'abc'
+          @user.save!
+          }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+  end
+
 end
 
