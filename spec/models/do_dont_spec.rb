fcef5435925acc7526243f564f41ae83b439eb8f
= <=>
2013-11-20 16:14:28 -0800

    Tests are also Dodonts without underscore

--- spec/models/do_dont_spec.rb
+++ spec/models/do_dont_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe DoDont do
+describe Dodont do
   # let(:dodont) { DoDont.create(content: "Hi Hu" ) }
   it "default value of dos is set to 0" do
-    do_dont = DoDont.new
-    do_dont.content = "Hi Hu"
-    do_dont.save
-    expect(do_dont.dos).to eq 0
+    dodont = Dodont.new
+    dodont.content = "Hi Hu"
+    dodont.save
+    expect(dodont.dos).to eq 0
   end
 
   it { should validate_presence_of(:content) }
