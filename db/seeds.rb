49ea80b73fc3344a52d2493d6a344a3c31e70e0f
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-11-22 10:30:12 -0800

    Added data to seeds.rb, so that categories can be set up easily using rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+text = Category.new
+text.name = "Text"
+text.instructions = "Enter your text into the box below:"
+text.save
+image = Category.new
+image.name = "Image"
+image.instructions = "Enter the YouTube Video's link into the text box below:"
+image.save
+youtube_video = Category.new
+youtube_video.name ="YouTube Video"
+youtube_video.instructions = "Enter the YouTube Video's link into the text box below:"
+youtube_video.save
+a_dodont = Dodont.new
+a_dodont.content = 'http://upload.wikimedia.org/wikipedia/commons/e/e1/Red_Squirrel_-_Lazienki.JPG'
+a_dodont.category_id = 2
+a_dodont.save
