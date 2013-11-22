6eb0a0b11204b74b4de7c22054d69fb3fb21d46c
= <=>
2013-11-22 14:53:05 -0800

    Refactored some new dodont model tests

--- spec/models/dodont_spec.rb
+++ spec/models/dodont_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe Dodont do
-  it "default value of dos is set to 0" do
+  let(:category) {
     category = Category.new
     category.name = 'something'
     category.instructions = 'something more'
     category.save
+    category
+  }
+
+  let(:category_image) {
+    category = Category.new
+    category.name = 'image'
+    category.instructions = 'blah blah'
+    category.save
+    category
+  }
+
+  let(:category_video) {
+    category = Category.new
+    category.name = 'youtube video'
+    category.instructions = 'blah blah'
+    category.save
+    category
+  }
+
+  it "default value of dos is set to 0" do
     dodont = Dodont.new
     dodont.content = "Hi Hu"
     dodont.category_id = category.id
@@ -16,10 +36,6 @@ describe Dodont do
   it { should validate_presence_of(:content) }
 
   it "rejects content that contains markup" do
-    category = Category.new
-    category.name = 'something'
-    category.instructions = 'something more'
-    category.save
     dodont = Dodont.new
     dodont.content = "<h1>Hi</h1>"
     dodont.category_id = category.id
@@ -28,37 +44,26 @@ describe Dodont do
   end
 
   it "adds markup on save if category's name is 'image'" do
-    category = Category.new
-    category.name = 'image'
-    category.instructions = 'blah blah'
-    category.save
+
     dodont = Dodont.new
     dodont.content = 'some_link'
-    dodont.category_id = category.id
+    dodont.category_id = category_image.id
     dodont.save
     expect(dodont.content).to eq "<img class='image' src='some_link'>"
   end
 
   it "adds markup to YouTube video link that is in long-form if category's name is 'youtube video'" do
-    category = Category.new
-    category.name = 'youtube video'
-    category.instructions = 'blah blah'
-    category.save
     dodont = Dodont.new
     dodont.content = "http://www.youtube.com/watch?v=R7AXBOT8KzU"
-    dodont.category_id = category.id
+    dodont.category_id = category_video.id
     dodont.save
     expect(dodont.content).to eq "<iframe class='video' src='//www.youtube.com/embed/R7AXBOT8KzU' frameborder='0' allowfullscreen></iframe>"
   end
 
   it "adds markup to YouTube video link that is in short-form if category's name is 'youtube video'" do
-    category = Category.new
-    category.name = 'youtube video'
-    category.instructions = 'blah blah'
-    category.save
     dodont = Dodont.new
     dodont.content = "http://youtu.be/R7AXBOT8KzU"
-    dodont.category_id = category.id
+    dodont.category_id = category_video.id
     dodont.save
     expect(dodont.content).to eq "<iframe class='video' src='//www.youtube.com/embed/R7AXBOT8KzU' frameborder='0' allowfullscreen></iframe>"
   end
